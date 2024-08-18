c10133075269a4bf2abcd04e9346b07a4209473c
youone04 <yudi.gunaone87@gmail.com>
2024-08-18 09:14:58 +0700

    modif banner and header

--- src/ComponentPositivus/Banner.jsx
+++ src/ComponentPositivus/Banner.jsx
@@ -1,16 +1,20 @@
 import '@/ComponentPositivus/assets/banner.css';
+import ilustrastion from '@/ComponentPositivus/assets/images/Illustration.png';
 export default function Banner() {
     return (
-        <div className='d-flex'>
-            <div className='flex-1' style={{background:'red'}}>
-                <h2>Navigating the digital landscape for success</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed natus iusto nesciunt,
-                    aperiam neque commodi sint velit. Reiciendis voluptatum, nobis blanditiis
-                    eius, laboriosam voluptatibus commodi, quidem neque modi obcaecati aspernatur.</p>
+        <div className='d-flex' style={{ marginTop: 50 }}>
+            <div className='flex-1'>
+                <div className='desc-positivus'>
+                    <h2>Navigating the digital landscape for success</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed natus iusto nesciunt,
+                        aperiam neque commodi sint velit. Reiciendis voluptatum, nobis blanditiis
+                        eius, laboriosam voluptatibus commodi, quidem neque modi obcaecati aspernatur.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed natus iusto nesciunt.</p>
+                    <button className='btn-positivus'>Book a consultation</button>
+                </div>
             </div>
-            <div className='flex-1' style={{background:'blue'}}>
-                <h1>Positivus</h1>
-
+            <div className='flex-1'>
+                <img src={ilustrastion} alt="ilustrastion" height={515} width={600.46} />
             </div>
         </div>
     )
--- src/ComponentPositivus/Header.jsx
+++ src/ComponentPositivus/Header.jsx
@@ -1,11 +1,15 @@
 import '@/ComponentPositivus/assets/index.css';
 import '@/ComponentPositivus/assets/header.css';
+import logoPositivius from '@/ComponentPositivus/assets/images/logo-positivius.png';
+
 
 export default function Header() {
     return (
         <div className='justifiy-content-center align-items-center d-flex'>
                 <div className='row-mod' style={{ width: '100%', gap: 40 }}>
-                    <h1 className='flex-1' style={{ paddingTop: 10, marginTop: 10 }}>LEFT</h1>
+                    <h1 className='flex-1' style={{ paddingTop: 10, marginTop: 10 }}>
+                        <img src={logoPositivius} alt='logo' />
+                    </h1>
                     <div className='flex-3'>
                         <ul className='horizontal-li' style={{ paddingTop: 20, marginTop: 10 }}>
                             <li>About us</li>
@@ -15,15 +19,16 @@ export default function Header() {
                             <li>Blog</li>
                         </ul>
                     </div>
-                    <div 
+                    <button 
                     className='justifiy-content-center align-items-center d-flex'
                     style={{
                         marginTop: 10,
-                        height: 55,
-                        width: 150,
+                        height: 68,
+                        width: 231,
                         border: '1px solid black',
-                        borderRadius: 10
-                    }}>Request a Quote</div>
+                        borderRadius: 14,
+                        backgroundColor: 'white',
+                    }}>Request a Quote</button>
 
 
                 </div>
