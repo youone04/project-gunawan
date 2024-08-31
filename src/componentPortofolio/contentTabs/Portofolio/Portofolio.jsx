5cbd5d40af9235b051bd193d069c38675eeb61a9
youone04 <yudi.gunaone87@gmail.com>
2024-08-31 23:31:11 +0700

    addjust

--- src/componentPortofolio/contentTabs/Portofolio/Portofolio.jsx
+++ src/componentPortofolio/contentTabs/Portofolio/Portofolio.jsx
@@ -6,7 +6,7 @@ export default function Portfolio() {
         <article className="portfolio active" data-page="portfolio">
 
         <header>
-          <h2 className="h2 article-title" style={{ color: 'white' }}>Portfolio</h2>
+          <h2 className="h2 article-title" style={{ color: 'white' }}>Portofolio</h2>
         </header>
 
         <section className="projects">
